2f7dfd96ad3ee64b35d1333a83c5036079044ada
Marlon Vella <marlon.vella@live.com>
2020-05-21 21:08:40 +0200

    services

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
   selector: 'pm-products',
@@ -22,33 +23,11 @@ export class ProductListComponent implements OnInit {
   }
 
   filteredProducts: Product[];
-  products: Product[] = [
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2019",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2019",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    }
-  ];
+  products: Product[] = [];
 
-  constructor() {
-    this.filteredProducts = this.products;
-    this.listFilter = 'cart';
-  }
+  constructor(
+    private productService: ProductService
+  ) {}
 
   onRatingClicked(message: string): void {
     this.pageTitle = 'Product List: ' + message;
@@ -57,7 +36,7 @@ export class ProductListComponent implements OnInit {
   performFilter(filterBy: string): Product[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.products.filter((product: Product) =>
-      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
   }
 
   toggleImage(): void {
@@ -65,6 +44,7 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('In OnInit');
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 }
